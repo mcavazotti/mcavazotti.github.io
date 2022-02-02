78bbb82194e29d17a934d3c0f3f7f7a69ae738e4
Matheus Cavazotti <mcavazotti@gmail.com>
2022-02-02 17:37:07 -0300

    project detail component + ui fixes

--- src/app/components/project-detail/project-detail.component.ts
+++ src/app/components/project-detail/project-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from 'src/app/definitions/project.inteface';
+import { TranslationHelper } from 'src/app/helpers/translation-helper';
+import { TranslationService } from 'src/app/services/translation.service';
 
 @Component({
   selector: 'app-project-detail',
@@ -7,13 +9,27 @@ import { Project } from 'src/app/definitions/project.inteface';
   styleUrls: ['./project-detail.component.css']
 })
 export class ProjectDetailComponent implements OnInit {
+  private translationHelper?: TranslationHelper;
+
   @Input() project: Project = {
     id: ''
   };
 
-  constructor() { }
+  name: string = '';
+  briefing: string = '';
+
+  constructor(private translationService: TranslationService) { }
 
   ngOnInit(): void {
+    var translationFile = "projects/" + this.project.id;
+
+    if (this.project.translationFile)
+      translationFile = this.project.translationFile;
+
+    this.translationHelper = new TranslationHelper(translationFile,this.translationService,(translation)=> {
+      this.name = translation.name;
+      this.briefing = translation.briefing;
+    });
   }
 
 }
