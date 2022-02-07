dcd312b2469ac1a14a61c97c89d9fa43169ef570
mcavazotti <matheus.d.cavazotti@exxonmobil.com>
2022-02-07 11:12:18 -0300

    yet more small fixes

--- convert-hjson.sh
+++ convert-hjson.sh
@@ -2,5 +2,5 @@
 for i in $(find ./src/assets/ -name "*.hjson" -type f); 
 do 
     f="${i%.hjson}"; ./hjson-cli -j $i > "${f}.json"
-    echo "converting ${i}  -> ${f}"
+    echo "converting ${i} -> ${f}.json"
 done
