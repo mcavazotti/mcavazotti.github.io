2cf1759bd1d2573189f4aca4159fd319a4179dcc
Matheus Cavazotti <mcavazotti@gmail.com>
2022-07-06 13:07:31 -0300

    navigation and theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PlaygroundPageComponent } from './pages/playground-page/playground-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomePageComponent},
+  {path:"blog", component: BlogPageComponent},
+  {path:"projects", component: ProjectsPageComponent},
+  {path:"about", component: AboutPageComponent},
+  {path:"playground", component: PlaygroundPageComponent},
+  {path:"", redirectTo:"home", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { PlaygroundPageComponent } from './pages/playground-page/playground-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    BlogPageComponent,
+    ProjectsPageComponent,
+    AboutPageComponent,
+    PlaygroundPageComponent
   ],
   imports: [
     BrowserModule,
