cc91071f8d3ac5ea31a12259683ab2c0d8ef87dd
Matheus Cavazotti <mcavazotti@gmail.com>
2022-07-10 17:50:30 -0300

    planets in angular

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SimController, Vector2 } from 'src/submodules/planets/src/simulator';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+  planetSim!: SimController;
+  constructor() {
+  }
 
   ngOnInit(): void {
+    let canvas: HTMLCanvasElement = (document.getElementById("planets")! as HTMLCanvasElement);
+    this.fitToContainer(canvas);
+
+    this.planetSim = new SimController("planets", new Vector2(500, 0), true, false);
+    this.planetSim.startSim();
+  }
+
+  private fitToContainer(canvas: HTMLCanvasElement) {
+    // Make it visually fill the positioned parent
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    // ...then set the internal size to match
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
   }
 
 }
