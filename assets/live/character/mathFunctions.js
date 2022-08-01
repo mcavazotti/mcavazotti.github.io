51421235a139945caf7db1bd1b31e1aa7c495e13
mcavazotti <mcavazotti@users.noreply.github.com>
2022-08-01 19:15:42 +0000

    Deploying to gh-pages from @ mcavazotti/mcavazotti.github.io@1747c1f250a7f3a07ac75bb94a9429f01419f76c 🚀

--- assets/live/character/mathFunctions.js
+++
@@ -1,7 +0,0 @@
-function convertDegreeToRadian(angle) {
-    return angle * Math.PI / 180;
-}
-
-function convertRadianToDegree(angle) {
-    return angle * 180 / Math.PI;
-}
---
+++ content/articles/a/a.md
@@ -0,0 +1,10 @@
+---
+id: a
+title: a Title
+date: "20220704"
+tags: [c]
+relatedProjects: [test]
+---
+
+# a Title
+1 2 3 4
