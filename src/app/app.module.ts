b5baf432717b5af3dffcf37716533b5e4f9b5036
mcavazotti <matheus.d.cavazotti@exxonmobil.com>
2022-10-05 09:22:29 -0300

    add bootstrap + a little styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { PagesModule } from './pages/pages.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { PagesModule } from './pages/pages.module';
     AppRoutingModule,
     ScullyLibModule,
     PagesModule,
+    FontAwesomeModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
