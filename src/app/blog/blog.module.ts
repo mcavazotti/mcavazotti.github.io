2c4a74ff8d652b759d9915af74fd02f9b734819c
mcavazotti <matheus.d.cavazotti@exxonmobil.com>
2022-10-05 09:22:52 -0300

    blog list

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,9 +3,10 @@ import {NgModule} from '@angular/core';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './blog.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, BlogListComponent],
   imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
 })
 export class BlogModule {}
