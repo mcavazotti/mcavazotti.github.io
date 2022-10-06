49bdcdd3a45f16376df031ce94dee334078fe6d7
mcavazotti <matheus.d.cavazotti@exxonmobil.com>
2022-10-06 10:37:12 -0300

    Home page done

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLinkedinIn, faGithubAlt, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+
+  linkedInIcon = faLinkedinIn;
+  githubIcon = faGithubAlt;
+  instagramIcon = faInstagram;
 
   constructor() { }
 
